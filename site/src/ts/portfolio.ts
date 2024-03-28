51dd785e8c0422c0469c43f0e868980a8aa99ce1
AysonAudio <contact@aysonaudio.dev>
2024-03-27 22:14:32 -0700

    feat: design and program SPA pagination

--- site/src/ts/portfolio.ts
+++ site/src/ts/portfolio.ts
@@ -5,12 +5,23 @@
 /** Cache of elems manipulated by app */
 type ElemCache = {
     tabBtns: NodeListOf<HTMLAnchorElement>;
+
+    reelPageBtns: NodeListOf<HTMLAnchorElement>;
+    projectPageBtns: NodeListOf<HTMLAnchorElement>;
+    musicPageBtns: NodeListOf<HTMLAnchorElement>;
+    compositionPageBtns: NodeListOf<HTMLAnchorElement>;
+
     sections: {
         reels: HTMLElement;
         projects: HTMLElement;
         music: HTMLElement;
         compositions: HTMLElement;
     };
+
+    reelPages: NodeListOf<HTMLElement>;
+    projectPages: NodeListOf<HTMLElement>;
+    musicPages: NodeListOf<HTMLElement>;
+    compositionPages: NodeListOf<HTMLElement>;
 };
 
 /**
@@ -21,12 +32,25 @@ type ElemCache = {
 export const GetElemCache: () => ElemCache = (() => {
     let cache: ElemCache = {
         tabBtns: document.querySelectorAll("#tabs > .tabs > ul > li > a"),
+
+        reelPageBtns: document.querySelectorAll("[id^='btn-reel-']"),
+        projectPageBtns: document.querySelectorAll("[id^='btn-project-']"),
+        musicPageBtns: document.querySelectorAll("[id^='btn-music-']"),
+        compositionPageBtns: document.querySelectorAll(
+            "[id^='btn-composition-']"
+        ),
+
         sections: {
             reels: document.querySelector("#reels"),
             projects: document.querySelector("#projects"),
             music: document.querySelector("#music"),
             compositions: document.querySelector("#compositions"),
         },
+
+        reelPages: document.querySelectorAll("[id^='reel-']"),
+        projectPages: document.querySelectorAll("[id^='project-']"),
+        musicPages: document.querySelectorAll("[id^='music-']"),
+        compositionPages: document.querySelectorAll("[id^='composition-']"),
     };
     return () => cache;
 })();
@@ -43,7 +67,6 @@ function initTabBtn(tabBtn: HTMLAnchorElement) {
     tabBtn.addEventListener("click", () => {
         for (const id in cache.sections) {
             const section: HTMLElement = cache.sections[id];
-
             if (id == targetSectionId) section.classList.remove("is-hidden");
             else section.classList.add("is-hidden");
         }
@@ -54,12 +77,58 @@ function initTabBtn(tabBtn: HTMLAnchorElement) {
     });
 }
 
+/** Make a page button hide / unhide content sections and set active page */
+function initPageBtn(pageBtn: HTMLAnchorElement) {
+    let cache = GetElemCache();
+    const targetPageId = pageBtn.id.slice("btn-".length);
+    const targetPageType = targetPageId.slice(0, targetPageId.indexOf("-"));
+    const targetPageNum = targetPageId.slice(targetPageId.indexOf("-") + 1);
+    const targetPageBtnList = targetPageType + "PageBtns";
+    const targetPagesList = targetPageType + "Pages";
+
+    pageBtn.addEventListener("click", () => {
+        for (const elem of cache[targetPagesList] as NodeListOf<HTMLElement>) {
+            if (elem.id == targetPageId)
+                for (const child of elem.children)
+                    child.classList.remove("is-hidden");
+            else
+                for (const child of elem.children)
+                    child.classList.add("is-hidden");
+        }
+
+        for (const _pageBtn of cache[targetPageBtnList]) {
+            _pageBtn.classList.remove("is-current");
+            _pageBtn.setAttribute(
+                "aria-label",
+                "Goto page " + (targetPageNum + 1)
+            );
+            _pageBtn.removeAttribute("aria-current");
+        }
+        pageBtn.classList.add("is-current");
+        pageBtn.setAttribute("aria-label", "Page " + (targetPageNum + 1));
+        pageBtn.setAttribute("aria-current", "page");
+    });
+}
+
 // ##################################################################### //
 // ################################ MAIN ############################### //
 // ##################################################################### //
 
 document.addEventListener("DOMContentLoaded", () => {
     let cache = GetElemCache();
-    for (const tab of cache.tabBtns) initTabBtn(tab);
-    cache.tabBtns[0].click();
+
+    for (const tabBtn of cache.tabBtns) initTabBtn(tabBtn);
+    cache.tabBtns[0]?.click();
+
+    for (const pageBtn of cache.reelPageBtns) initPageBtn(pageBtn);
+    cache.reelPageBtns[0]?.click();
+
+    for (const pageBtn of cache.projectPageBtns) initPageBtn(pageBtn);
+    cache.projectPageBtns[0]?.click();
+
+    for (const pageBtn of cache.musicPageBtns) initPageBtn(pageBtn);
+    cache.musicPageBtns[0]?.click();
+
+    for (const pageBtn of cache.compositionPageBtns) initPageBtn(pageBtn);
+    cache.compositionPageBtns[0]?.click();
 });
